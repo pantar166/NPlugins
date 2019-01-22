11bb109240a8b90cba950294ebdf7398130f7d37
neoskr <42131095+neoskr@users.noreply.github.com>
2019-01-22 20:20:36 +0900

    I created and removed some sources
    ;)

--- NEntityTag.php
+++ NEntityTag.php
@@ -1,27 +1,31 @@
 namespace NEntityTag;
+class NEntityTag extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\entity\Entity;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\event\entity\EntityDamageEvent;
-
-class NEntityTag extends PluginBase implements Listener {
-
-public function onEnable () {
-    $this->getServer()->getPluginManager()->registerEvents ($this,$this);
-}
-
-public function setNameTag (EntityDamageEvent $event) {
-    $entity = $event->getEntity();
-    if ($entity instanceof Player) {
-    } else {
-      $tag = explode("\n", $entity->getNameTag()) [0];
-      $text = '§c§lHP ∥ §r§f' . $entity->getHealth() . '§7 / ' . $entity->getMaxHealth();
-      $entity->setNameTag($tag . "\n" . $text);
-      $entity->setNameTagVisible(true);
-      $entity->setNameTagAlwaysVisible(true);
+    public function onEnable () {
+        $this->getServer()->getPluginManager()->registerEvents ($this,$this);
     }
-  }
+
+    public function setNameTag ($entity) {
+        $tag = explode("\n", $entity->getNameTag()) [0];
+        $text = '§c§lHP ∥ §r§f' . $entity->getHealth() . '§7 / ' . $entity->getMaxHealth();
+        $entity->setNameTag($tag . "\n" . $text);
+        $entity->setNameTagVisible(true);
+        $entity->setNameTagAlwaysVisible(true);
+    }
+
+    public function onDamage (\pocketmine\event\entity\EntityDamageEvent $event) {
+        $entity = $event->getEntity();
+        if ($entity instanceof \pocketmine\Player) {
+        } else {
+            $this->setNameTag($entity);
+        }
+    }
+
+    public function onSpawn (\pocketmine\event\entity\EntityDamageEvent $event) {
+        $entity = $event->getEntity();
+        if ($entity instanceof \pocketmine\Player) {
+        } else {
+            $this->setNameTag($entity);
+        }
+    }
 }
